ff1130bdd3053f97b79b6c636a5191be58f71713
Nurbol <nurbol_221@mail.ru>
2024-06-23 16:48:53 +0500

    Реализована запись и статистика результата игр

---
+++ MovieQuiz/Models/QuizQuestion.swift
@@ -0,0 +1,14 @@
+//
+//  QuizQuestion.swift
+//  MovieQuiz
+//
+//  Created by Nurbol on 19.06.2024.
+//
+
+import Foundation
+
+struct QuizQuestion {
+    let image: String
+    let text: String
+    let correctAnswer: Bool
+}
---
+++ MovieQuiz/Models/QuizStepViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  QuizStepViewModel.swift
+//  MovieQuiz
+//
+//  Created by Nurbol on 19.06.2024.
+//
+
+import UIKit
+
+struct QuizStepViewModel {
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+}
